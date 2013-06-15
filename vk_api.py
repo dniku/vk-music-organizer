f5bdc85e91050be5cbeb0f4fcaf814ac926a91a4
Aleksandr Mikheev <qqalexqq@yandex.ru>
2013-06-16 03:42:56 +0400

    A little cleanup in code and error messages.
    I did a little cleanup in the vk_api and vk_auth files. Now they look
    more pythonic and readable. Also I edited error messages texts.

--- vk_api.py
+++ vk_api.py
@@ -8,19 +8,18 @@ last_time = 0.0
 def call_api(method, params, token):
 	global last_time
 
-	if isinstance(params, list):
-		params_list = [kv for kv in params]
-	elif isinstance(params, dict):
+	# Make timeout between api requests retries
+	time.sleep(max(0.0, 0.3333 - (time.clock() - last_time)))
+
+	if type(params) == type(dict):
 		params_list = params.items()
 	else:
-		params_list = [params]
+		params_list = list(params)
 
-	params_list.append(("access_token", token))
+	params_list += [("access_token", token)]
 	url = "https://api.vk.com/method/%s?%s" % (method, urlencode(params_list))
 
-	if time.clock() - last_time <= 0.3333:
-		time.sleep(max(0.0, 0.3333 - (time.clock() - last_time)))
 	response = urllib2.urlopen(url).read()
 	last_time = time.clock()
 
-	return json.loads(response)["response"]
+	return json.loads(response)['response']
