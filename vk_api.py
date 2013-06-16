bef2c44150396e6ad56222c2c652a844b7a1ce62
Pastafarianist <mr.pastafarianist@gmail.com>
2013-06-16 20:10:27 +0400

    Fixed crash and indentation
    Gonna think twice before accepting another pull request

--- vk_api.py
+++ vk_api.py
@@ -6,22 +6,22 @@ from urllib import urlencode
 last_time = 0.0
 
 def call_api(method, params, token):
-    global last_time
+	global last_time
 
-    # Make timeout between api requests retries
-    time.sleep(max(0.0, 0.3333 - (time.clock() - last_time)))
+	# Make timeout between api requests retries
+	time.sleep(max(0.0, 0.3333 - (time.clock() - last_time)))
 
-    if isinstance(params, list):
-        params_list = params[:]
-    elif isinstance(params, dict):
-        params_list = params.items()
-    else:
-        params_list = [params]
+	if isinstance(params, list):
+		params_list = params[:]
+	elif isinstance(params, dict):
+		params_list = params.items()
+	else:
+		params_list = [params]
 
-    params_list += [('access_token', token)]
-    url = 'https://api.vk.com/method/%s?%s' % (method, urlencode(params_list))
+	params_list += [('access_token', token)]
+	url = 'https://api.vk.com/method/%s?%s' % (method, urlencode(params_list))
 
-    response = urllib2.urlopen(url).read()
-    last_time = time.clock()
+	response = urllib2.urlopen(url).read()
+	last_time = time.clock()
 
-    return json.loads(response)['response']
+	return json.loads(response)['response']
